ce9b9b3ab1c1283e5150f5970cbde50b7c120503
Rui Leite <ruihloliveira@gmail.com>
2021-06-15 20:24:20 -0300

    Melhoria no design; Implementação de Menu; Rodapé;

--- ScreenComponents/Home/Home.js
+++ ScreenComponents/Home/Home.js
@@ -4,16 +4,10 @@ import Post from "./Post.js";
 export default {
     data: function () {
         return {
-            notebooks: [],
-            createdNotebook: null,
-            editFormActive: false,
-            createFormActive: false,
-            notebookForEditing: {},
-            showCompletedNotebooks: false,
-            activeNotebook: {},
+            menuLinks: [],
+            postList: [],
             post: {},
             postFile: '',
-            postList: [],
         }
     },
     watch: {
@@ -26,6 +20,14 @@ export default {
         // 'NotesListing':NotesListing,
     },
     methods: {
+        async loadLinks(){
+            let result = await fetch("./../../links/menuLinks.json")
+            .then((response) => {
+                return response.json();
+            });
+
+            this.menuLinks = result;
+        },
         async loadPosts(){
             let result = await fetch("./../../posts/postList.json")
             .then((response) => {
@@ -40,7 +42,6 @@ export default {
                 result[i]['post'] = post;
             }
             this.postList = result;
-            console.log(this.postList);
         },
         openPost(postFile){ //file e post
             const post = this.postList.find(function (element) {
@@ -55,41 +56,57 @@ export default {
         }
     },
     created () {
-        // this.loadNotebooks();
+        this.loadLinks()
         this.loadPosts();
     },
     template: `
-    <div>
+    <div class="mainContainer flex f-row f-justify-center f-alignitems-stretch f-sons-flex-1">
         <link rel="stylesheet" href="./ScreenComponents/Home/home.css">
         
-        <div class="">
-            <div class="flex f-column f-alignitems-center mt-10 mb-30">
-                <div class="blog-title mb-10"><h1>&lt;rui-leite /&gt;</h1></div>
-                <div class="blog-subtitle mb-10"><h6>Blog e estudos</h6></div>
-                <div>links</div>
+        <div class="mainContainerDecoration blog-postList-max-width">
+        
+            <!-- HEADER -->
+            <div class="flex f-column f-alignitems-stretch mt-10 mb-30">
+                <div class="text-center blog-title mb-20"><h1>&lt;rui-leite /&gt;</h1></div>
+                <div class="text-center blog-subtitle mb-15"><h6>Blog e estudos</h6></div>
+                <div class="mt-15 blog-link-list flex f-row f-wrap-wrap f-justify-spaceAround">
+                    <div class="blog-link"
+                        v-for="link in menuLinks"
+                    >
+                        <a v-if="link.type == 1" href="#" @click="openPost(link.url)">{{link.name}}</a>
+                        <a v-if="link.type == 2" :href="link.url" target="_blank">{{link.name}}</a>
+                    </div>
+                </div>
             </div>
             
+            <!-- POST VIEWER -->
             <div class="flex f-row f-justify-center mt-30 f-sons-flex-1">
-                <Post 
-                    class="blog-postList-max-width"
+                <Post class=""
                     v-bind:post="post"
                     v-bind:file="postFile"
                     v-on:closePost="closePost($event)"
                 ></Post>
             </div>
             
+            <!-- POST LIST -->
             <div class="flex f-row f-justify-center mt-30 f-sons-flex-1">
-                <div class="blog-postList-max-width">
-                    <h2 class="post-title">Posts</h2><hr>
+                <div class="">
+                    <h2 class="postList-title">Posts</h2><hr>
                     <div v-for="post in postList"
                         @click="openPost(post.file)"
-                        class="flex f-column post-title post-title-link cursor-pointer pl-10 pr-10 mt-15 pt-10 pb-10"
+                        class="flex f-column postList-title-link cursor-pointer pl-10 pr-10 mt-15 pt-10 pb-10"
                     >
-                        <span>{{post.post.title}}</span>
-                        <span class="mt-10 post-author-datetime">{{post.post.date}}</span>
+                        <span class="postList-title">{{post.post.title}}</span>
+                        <span class="mt-5 postList-author-datetime">{{post.post.date}}</span>
                     </div>
                 </div>
             </div>
+            
+            <!-- FOOTER -->
+            <div class="flex f-column f-alignitems-center mt-30">
+                <div class="blog-footer">Feito com amor pelo PHP mais cansado da galacta "menor que três"</div>
+            </div>
+            
             <notice-box></notice-box>
         </div>
     </div>
--- ScreenComponents/Home/Post.js
+++ ScreenComponents/Home/Post.js
@@ -14,41 +14,29 @@ export default {
     components: {
     },
     methods: {
-        // async loadPosts(){
-        //     let result = await fetch(`./../../posts/${this.parameters.p}`)
-        //     .then((response) => {
-        //         return response.json();
-        //     });
-        //     console.log('result',result);
-        //     this.post = result;
-        // }
         closePost () {
             this.$emit('closePost');
         }
     },
     created () {
         console.log('inner post', this.post);
-        // this.loadNotebooks();
-        // this.loadPosts();
     },
     template: `
     <div class="">
-        <div v-if="post.title">
-            <div class="flex f-column f-alignitems-center">
-                <div class="post-title"><span v-html="post.title"></span></div>
-                <div class="post-subtitle"><span v-html="post.subtitle"></span></div>
-                <div class="post-authordate"><span v-html="post.author"></span> @ <span v-html="post.date" ></span></div>
+        <transition name="fadeInDown">
+            <div v-if="post.title">
+                <div class="flex f-column f-alignitems-center">
+                    <div class="post-title mt-5 mb-5"><span v-html="post.title"></span></div>
+                    <div class="post-subtitle mt-5 mb-5"><span v-html="post.subtitle"></span></div>
+                    <div class="post-date mt-5 mb-5"><span v-html="post.date" ></span></div>
+                </div>
+                <div class="mt-15 mb-15 flex f-row f-justify-end"><button class="btn btn-black" type="button" @click="closePost()">Voltar</button></div>
+                <div class="post-body">
+                    <p v-for="paragraph in post.body" v-html="paragraph"></p>
+                </div>
+                <div class="mt-15 mb-15 flex f-row f-justify-end"><button class="btn btn-black" type="button" @click="closePost()">Voltar</button></div>
             </div>
-            
-            <div class="mt-15 mb-15 flex f-row f-justify-end"><button class="btn btn-black" type="button" @click="closePost()">Close Post</button></div>
-            
-            <div class="post-body">
-                <p v-for="paragraph in post.body" v-html="paragraph"></p>
-            </div>
-            
-            <div class="mt-15 mb-15 flex f-row f-justify-end"><button class="btn btn-black" type="button" @click="closePost()">Close Post</button></div>
-            
-        </div>
+        </transition>
     </div>
     `
 };
