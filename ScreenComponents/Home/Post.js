7ff725ee8fe715ab9bfa96ad34368ff75d1708ca
Rui Leite <ruihloliveira@gmail.com>
2021-06-28 20:06:07 -0300

    Post Never Use Arrays

--- ScreenComponents/Home/Post.js
+++ ScreenComponents/Home/Post.js
@@ -16,6 +16,11 @@ export default {
     methods: {
         closePost () {
             this.$emit('closePost');
+        },
+        processCode (paragraph) {
+            paragraph = paragraph.replace("<code>","");
+            paragraph = paragraph.replace("</code>","");
+            return paragraph;
         }
     },
     created () {
@@ -27,12 +32,15 @@ export default {
             <div v-if="post.title">
                 <div class="flex f-column f-alignitems-center">
                     <div class="post-title mt-5 mb-5"><span v-html="post.title"></span></div>
-                    <div class="post-subtitle mt-5 mb-5"><span v-html="post.subtitle"></span></div>
+                    <div class="post-subtitle mt-5 mb-5 text-center"><span v-html="post.subtitle"></span></div>
                     <div class="post-date mt-5 mb-5"><span v-html="post.date" ></span></div>
                 </div>
                 <div class="mt-15 mb-15 flex f-row f-justify-end"><button class="btn btn-black" type="button" @click="closePost()">Voltar</button></div>
                 <div class="post-body">
-                    <p v-for="paragraph in post.body" v-html="paragraph"></p>
+                    <p v-for="paragraph in post.body" >
+                        <span v-if="paragraph.includes('<code>')" class="code-container">{{processCode(paragraph)}}</span>
+                        <span v-else v-html="paragraph"></span>
+                    </p>
                 </div>
                 <div class="mt-15 mb-15 flex f-row f-justify-end"><button class="btn btn-black" type="button" @click="closePost()">Voltar</button></div>
             </div>
